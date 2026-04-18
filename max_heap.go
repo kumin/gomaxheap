e8698c468b896439d3cb3a5cdd432260f0914b29
minhhoang7 <minh.hoang@netapp.com>
2026-04-18 18:29:18 +0700

    add heapify, insert maxheap

---
+++ max_heap.go
@@ -0,0 +1,103 @@
+package maxheap
+
+import (
+	"log/slog"
+	"slices"
+)
+
+type Node struct {
+	Seed   any
+	Weight float64
+}
+
+type Heap struct {
+	nodes []*Node
+}
+
+func NewHeap(nodes []*Node) *Heap {
+	heap := &Heap{nodes: nodes}
+	buildHeap(heap.nodes)
+
+	return heap
+}
+
+func (h *Heap) Pop() *Node {
+	if h.Size() == 0 {
+		slog.Warn("heap is empty")
+		return nil
+	}
+
+	return h.nodes[0]
+}
+
+func (h *Heap) Insert(node *Node) {
+	h.nodes = append(h.nodes, node)
+	n := h.Size()
+	i := n - 1
+	for i > 0 {
+		parentIdx := getParentIdx(i)
+		if parentIdx*2+1 < n && h.nodes[parentIdx].Weight < h.nodes[parentIdx*2+1].Weight {
+			h.nodes[parentIdx], h.nodes[parentIdx*2+1] = h.nodes[parentIdx*2+1], h.nodes[parentIdx]
+		}
+		if parentIdx*2+2 < n && h.nodes[parentIdx].Weight < h.nodes[parentIdx*2+2].Weight {
+			h.nodes[parentIdx], h.nodes[parentIdx*2+2] = h.nodes[parentIdx*2+2], h.nodes[parentIdx]
+		}
+
+		i = parentIdx
+	}
+}
+
+func (h *Heap) Delete(nodeIdx int) {
+
+}
+
+func (h *Heap) Size() int {
+	return len(h.nodes)
+}
+
+func (h *Heap) GetNodes() []*Node {
+	return slices.Clone(h.nodes)
+}
+
+func buildHeap(nodes []*Node) {
+	n := len(nodes)
+	i := n - 1
+	for i > 1 {
+		parentIdx := getParentIdx(i)
+		heapify(parentIdx, nodes)
+		i -= 2
+	}
+}
+
+func (h *Heap) PrintHeap() {
+	slog.Info("=========HEAP NODES==========")
+	for i, node := range h.nodes {
+		slog.Info("node", slog.Int("idx", i), slog.Any("value", node))
+	}
+}
+
+func heapify(parentIdx int, nodes []*Node) {
+	n := len(nodes)
+	if parentIdx*2+1 < n && nodes[parentIdx].Weight < nodes[parentIdx*2+1].Weight {
+		nodes[parentIdx], nodes[parentIdx*2+1] = nodes[parentIdx*2+1], nodes[parentIdx]
+		heapify(parentIdx*2+1, nodes)
+	}
+	if parentIdx*2+2 < n && nodes[parentIdx].Weight < nodes[parentIdx*2+2].Weight {
+		nodes[parentIdx], nodes[parentIdx*2+2] = nodes[parentIdx*2+2], nodes[parentIdx]
+		heapify(parentIdx*2+2, nodes)
+	}
+}
+
+func getParentIdx(idx int) int {
+	if idx < 1 {
+		return idx
+	}
+	var parentIdx int
+	if idx%2 == 0 {
+		parentIdx = (idx - 2) / 2
+	} else {
+		parentIdx = (idx - 1) / 2
+	}
+
+	return parentIdx
+}
---
+++ max_heap_test.go
@@ -0,0 +1,63 @@
+package maxheap
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHeapify(t *testing.T) {
+	testCases := []struct {
+		name  string
+		nodes []*Node
+		heap  []*Node
+	}{
+		{
+			name:  "test case 1",
+			nodes: []*Node{{Seed: "kumin", Weight: 2}, {Seed: "midu", Weight: 10}, {Seed: "omachi", Weight: 3}, {Seed: "may", Weight: 6}, {Seed: "lulu", Weight: 4}},
+			heap:  []*Node{{Seed: "midu", Weight: 10}, {Seed: "may", Weight: 6}, {Seed: "omachi", Weight: 3}, {Seed: "kumin", Weight: 2}, {Seed: "lulu", Weight: 4}},
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			heap := NewHeap(tt.nodes)
+			heap.PrintHeap()
+			assert.Equal(t, heap.GetNodes(), tt.heap)
+		})
+	}
+}
+
+func TestInsert(t *testing.T) {
+	type args struct {
+		nodes        []*Node
+		insertedNode *Node
+		heap         []*Node
+	}
+
+	testCases := []struct {
+		name string
+		args *args
+	}{
+		{
+			name: "test case 1",
+			args: &args{
+				nodes: []*Node{{Seed: "kumin", Weight: 2}, {Seed: "midu", Weight: 10}, {Seed: "omachi", Weight: 3}, {Seed: "may", Weight: 6}, {Seed: "lulu", Weight: 4}},
+				insertedNode: &Node{
+					Seed:   "nhan",
+					Weight: 100,
+				},
+				heap: []*Node{{Seed: "nhan", Weight: 100}, {Seed: "may", Weight: 6}, {Seed: "midu", Weight: 10}, {Seed: "kumin", Weight: 2}, {Seed: "lulu", Weight: 4}, {Seed: "omachi", Weight: 3}},
+			},
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			heap := NewHeap(tt.args.nodes)
+			heap.Insert(tt.args.insertedNode)
+			heap.PrintHeap()
+			assert.Equal(t, heap.GetNodes(), tt.args.heap)
+		})
+	}
+}
